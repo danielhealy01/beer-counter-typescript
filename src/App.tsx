1bfebb23340d1771d3998ba019b1a685d37fa8da
DanielHealy <danielhealyhosting3@gmail.com>
2022-11-27 17:49:54 +0000

    Each drinker has their own state.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ function App() {
 
   const [volume, setVolume] = useState("pint");
   const [drinker, setDrinker] = useState("");
-  const [total, setTotal] = useState(0);
 
-
+  const [drinker1Total, setDrinker1Total] = useState(0)
+  const [drinker2Total, setDrinker2Total] = useState(0);
+  const [drinker3Total, setDrinker3Total] = useState(0);
+  const [drinker4Total, setDrinker4Total] = useState(0);
 
   const handleSetVolumePint = () => {
     setVolume("pint");
@@ -18,13 +20,72 @@ function App() {
   };
 
   const handleIncrease = () => {
-    volume === "pint" ? setTotal(total + 1) : setTotal(total + 0.5);
+    if (drinker === 'drinker1') {
+      volume === "pint"
+        ? setDrinker1Total(drinker1Total + 1)
+        : setDrinker1Total(drinker1Total + 0.5);
+    }
+
+    if (drinker === "drinker2") {
+      volume === "pint"
+        ? setDrinker2Total(drinker2Total + 1)
+        : setDrinker2Total(drinker2Total + 0.5);
+    }
+
+    if (drinker === "drinker3") {
+      volume === "pint"
+        ? setDrinker3Total(drinker3Total + 1)
+        : setDrinker3Total(drinker3Total + 0.5);
+    }
+
+    if (drinker === "drinker4") {
+      volume === "pint"
+        ? setDrinker4Total(drinker4Total + 1)
+        : setDrinker4Total(drinker4Total + 0.5);
+    }    
   };
 
   const handleDecrease = () => {
-    if (total > 0) {
-      volume === "half" ? setTotal(total - 0.5) : total >= 1 ? setTotal(total - 1) : null;
+    if (drinker === 'drinker1') {
+      if (drinker1Total > 0) {
+        volume === "half"
+          ? setDrinker1Total(drinker1Total - 0.5)
+          : drinker1Total >= 1
+          ? setDrinker1Total(drinker1Total - 1)
+          : null;
+      }
     }
+
+    if (drinker === 'drinker2') {
+      if (drinker2Total > 0) {
+        volume === "half"
+          ? setDrinker2Total(drinker2Total - 0.5)
+          : drinker2Total >= 1
+          ? setDrinker2Total(drinker2Total - 1)
+          : null;
+      }
+    }
+
+    if (drinker === "drinker3") {
+      if (drinker3Total > 0) {
+        volume === "half"
+          ? setDrinker3Total(drinker3Total - 0.5)
+          : drinker3Total >= 1
+          ? setDrinker3Total(drinker3Total - 1)
+          : null;
+      }
+    }
+
+    if (drinker === "drinker4") {
+      if (drinker4Total > 0) {
+        volume === "half"
+          ? setDrinker4Total(drinker4Total - 0.5)
+          : drinker4Total >= 1
+          ? setDrinker4Total(drinker4Total - 1)
+          : null;
+      }
+    }
+
   };
 
   const handleSetDrinker1 = () => {
@@ -79,7 +140,12 @@ function App() {
     <div className="background">
       <div className="container">
         <p className="counter" id="counter">
-          {total}
+          {drinker.length === 0 ? 'Time for a beer?' : null }
+          {drinker === 'drinker1' ? drinker1Total : null}
+          {drinker === 'drinker2' ? drinker2Total : null}
+          {drinker === 'drinker3' ? drinker3Total : null}
+          {drinker === 'drinker4' ? drinker4Total : null}
+
         </p>
         <div className="btnContainer">
           <div className="btn minus" onClick={handleDecrease}>
